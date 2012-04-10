8fb8dfebfa5f2cefa3fcc6434c87b483f5839527
Dominic <dominc@dtrsoft.com>
2012-04-09 17:54:44 -0700

    Initial project posting

---
+++ CS363Lab/Controllers/StoresController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CS363Lab.Models;
+
+namespace CS363Lab.Controllers
+{
+    public class StoresController : Controller
+    {
+        private DataRepository data = new DataRepository();
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            DataRepository data = new DataRepository();
+            var stores = data.GetAllStores();
+            return View(stores);
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Store store)
+        {
+            
+            aspnet_User user = data.GetCurrentUser();
+            data.AddStore(store, user);
+            return View("CreateSuccess", store);
+        }
+
+        [Authorize]
+        public ActionResult AddProducts()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult AddProducts(Product product)
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ CS363Lab/Models/DataRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace CS363Lab.Models
+{
+    public class DataRepository
+    {
+        private WebStoreDataDataContext webstoredata = new WebStoreDataDataContext();
+
+        public aspnet_User GetCurrentUser()
+        {
+            MembershipUser currentUser = Membership.GetUser();
+            var user = from users in webstoredata.aspnet_Users
+                               where users.UserName.Equals(currentUser.UserName)
+                               select users;
+            return (aspnet_User)user.First();
+        }
+
+        public int AddStore(Store store, aspnet_User user)
+        {
+            webstoredata.Stores.InsertOnSubmit(store);
+            webstoredata.SubmitChanges();
+            return store.StoreID;
+        }
+
+        public int AddProduct(Product product)
+        {
+            webstoredata.Products.InsertOnSubmit(product);
+            webstoredata.SubmitChanges();
+            return product.ProductID;
+        }
+
+        public IQueryable<Store> GetAllStores()
+        {
+            var store = from data in webstoredata.Stores
+                    select data;
+            return store;
+        }
+
+        public IQueryable<Product> GetProductsByStore(int storeid)
+        {
+            var product = from data in webstoredata.Products
+                          where data.ProductID == storeid
+                          select data;
+            return product;
+        }
+    }
+}
