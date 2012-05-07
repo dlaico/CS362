c7f898d454c846e45beb4f99d6789c380f715b16
Dominic <dominc@dtrsoft.com>
2012-05-07 12:07:18 -0700

    Added final controllers to project.

---
+++ CS363Lab/Controllers/ReportsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS363Lab.Controllers
+{
+    public class ReportsController : Controller
+    {
+        //
+        // GET: /Reports/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
